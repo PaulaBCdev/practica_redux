b8cc584bce4a5912bfe530299ac563cf7751af7c
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-08-07 11:12:19 +0200

    async action test

--- src/store/actions.spec.ts
+++ src/store/actions.spec.ts
@@ -1,4 +1,5 @@
-import { adsLoadedFulfilled, authLoginFulfilled } from "./actions";
+import type { Credentials } from "../pages/auth/types";
+import { adsLoadedFulfilled, authLogin, authLoginFulfilled } from "./actions";
 
 describe("authLoginFulfilled", () => {
   test('should return an "auth/login/fulfilled" action', () => {
@@ -27,5 +28,66 @@ describe("adsLoadedFulfilled", () => {
 
     expect(result).toEqual(expected);
     expect(result.payload).toHaveLength(1);
+  });
+});
+
+describe("authLogin", () => {
+  afterEach(() => {
+    dispatch.mockClear();
+    router.navigate.mockClear();
+  });
+
+  const credentials: Credentials = {
+    email: "paula@gmail.com",
+    password: "1234",
+  };
+  const thunk = authLogin(credentials, true);
+
+  // mocks
+  const dispatch = vi.fn();
+  const api = {
+    auth: {
+      login: vi.fn(),
+    },
+  };
+  const from = "/from";
+  const router = {
+    state: { location: { state: { from } } },
+    navigate: vi.fn(),
+  };
+
+  test("when login resolves", async () => {
+    api.auth.login = vi.fn().mockResolvedValue(undefined);
+    // @ts-expect-error: no need getState
+    await thunk(dispatch, undefined, { api, router });
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: "auth/login/pending" });
+    expect(dispatch).toHaveBeenNthCalledWith(2, {
+      type: "auth/login/fulfilled",
+    });
+
+    expect(api.auth.login).toHaveBeenCalledWith(credentials, true);
+
+    expect(router.navigate).toHaveBeenCalledWith(from, { replace: true });
+  });
+
+  test("when login rejects", async () => {
+    const error = new Error("unauthorized");
+
+    api.auth.login = vi.fn().mockRejectedValue(error);
+    await expect(() =>
+      // @ts-expect-error: no need getState
+      thunk(dispatch, undefined, { api, router }),
+    ).rejects.toThrowError(error);
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: "auth/login/pending" });
+    expect(dispatch).toHaveBeenNthCalledWith(2, {
+      type: "auth/login/rejected",
+      payload: error,
+    });
+
+    expect(router.navigate).not.toHaveBeenCalled();
   });
 });
